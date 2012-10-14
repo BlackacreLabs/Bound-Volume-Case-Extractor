ea8027c0a291eeafdd885af12236805b853dea66
Kyle Mitchell <kyleevanmitchell@gmail.com>
2012-10-13 23:33:41 -0500

    sort opinion start pages

--- extractor.rb
+++ extractor.rb
@@ -3,7 +3,7 @@ yaml = YAML::load_file(ARGV[0])
 volume_number = yaml['volume']
 bound_volume = ARGV[1]
 offset = yaml['offset']
-start_pages = yaml['opinions']['start']
+start_pages = yaml['opinions']['start'].sort
 start_pdf_pages = start_pages.map {|x| x + offset}
 end_pdf_pages = start_pdf_pages.map{|x| x - 1} 
 end_pdf_pages.shift
@@ -12,5 +12,6 @@ ranges = start_pdf_pages.zip(end_pdf_pages)
 ranges.each do |range|
   citation_page = start_pages.shift
   new_pdf = "#{volume_number} U.S. #{citation_page}.pdf"
+  puts "#{volume_number} U.S. #{citation_page}: #{range[0]}-#{range[1]}"
   `pdftk A='#{bound_volume}' cat A#{range[0]}-#{range[1]} output '#{new_pdf}'`
 end
